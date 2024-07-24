1bcceb10aa7e0bd12b669941844947dea6fa9e2b
BK-beep <baraka.khaoula0@gmail.com>
2024-07-24 12:20:45 +0100

    docker: kafka-zookeeper + es-kibana

---
+++ src/main/java/ma/attijari/complainstrackingsystem/ComplainsTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package ma.attijari.complainstrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplainsTrackingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplainsTrackingSystemApplication.class, args);
+    }
+
+}
