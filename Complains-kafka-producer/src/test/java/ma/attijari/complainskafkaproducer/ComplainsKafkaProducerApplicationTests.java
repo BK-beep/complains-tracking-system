60816c9ba3e07c6755d4f9d7295f171b1deb0a55
BK-beep <baraka.khaoula0@gmail.com>
2024-07-25 10:25:20 +0100

    config(git) + discovery services

---
+++ Complains-kafka-producer/src/test/java/ma/attijari/complainskafkaproducer/ComplainsKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.attijari.complainskafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplainsKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
